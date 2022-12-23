277e5cbb4337118ee02c6f32c9ce67d9abf5706b
Ivan Repiso Camara <irepisocamara@gmail.com>
2022-12-23 09:45:20 +0100

    User schema

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@”]+(\.[^<>()[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/,
+      'Email is not valid'
+    ]
+  },
+  displayName: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true,
+  versionKey: false
+})
+
+export default model('User', userSchema)
